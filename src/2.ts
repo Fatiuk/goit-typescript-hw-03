0941f4ac5fb6550c2ecc2b312f289585d28a1c6d
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-03 11:53:22 +0200

    feat: complete task 2

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,11 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +33,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
